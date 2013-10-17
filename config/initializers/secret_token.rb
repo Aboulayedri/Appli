3772b82e80fe466752458e03af42525dc7853319
Abdoulaye SOUMARE <abdoulaye.soumare@devoteam.com>
2013-10-17 11:30:29 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AppDri::Application.config.secret_key_base = '0b6b157e48b7aab21a9bec76400a02c09098ff1a4058e75e23f85918b79b5b9ea50a93064c5615fed47ec6211f93cee00749a3295c6e42c6b6fa54ff9f4a829a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AppDri::Application.config.session_store :cookie_store, key: '_app_dri_session'
